dc24e76ae6299e200c8fbdf79399aa5600fd1ceb
tonytang731 <50162066+tonytang731@users.noreply.github.com>
2019-11-26 15:19:27 -0500

    state data passing

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -3,11 +3,20 @@
 import React, { Component } from 'react';
 import { api } from '../api';
 
+const testData = {
+  postBy: 'Yiwen123',
+  title: 'testTitle',
+  description: 'I love mountains!',
+  // FIXME: Profile Image Missing!!
+};
+
 class Post extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { apiResponse: '' };
+    this.state = { 
+      data: testData,
+      apiResponse: '' };
 
     this.callAPI = this.callAPI.bind(this);
   }
@@ -24,37 +33,21 @@ class Post extends Component {
   }
 
   render() {
+    const {data} = this.state;
     return (
       <div className="uk-container uk-container-small">
         <div id="cards" className="uk-child-width-1-2@m uk-align-center uk-background-default">
           <div className="uk-card uk-card-default uk-card-hover uk-align-center" uk-scrollspy="cls: uk-animation-slide-left; repeat: true">
-            <h3 className="uk-card-title uk-text-left-medium">Post Title</h3>
+            <h3 className="uk-card-title uk-text-left-medium">{data.title}</h3>
             <div className="uk-card-media-top">
               <img src={require('../images/photogram.png')} alt="" />
             </div>
             <div className="uk-card-body">
               <h3 className="uk-card-title uk-text-small">
-                Posted by
-                {' '}
-                <a href="">Somebody</a>
+                Posted by {" "}
+                <a href="">{data.postBy}</a>
               </h3>
-              <p id="">I Love Mountains...</p>
-              <a href="" uk-icon="heart" />
-              <a href="" uk-icon="comments" />
-            </div>
-          </div>
-          <div className="uk-card uk-card-default uk-card-hover uk-align-center" uk-scrollspy="cls: uk-animation-slide-left; repeat: true">
-            <h3 className="uk-card-title uk-text-left-medium">Post Title</h3>
-            <div className="uk-card-media-top">
-              <img src={require('../images/photogram.png')} alt="" />
-            </div>
-            <div className="uk-card-body">
-              <h3 className="uk-card-title uk-text-small">
-                Posted by
-                {' '}
-                <a href="">Somebody</a>
-              </h3>
-              <p id="">I Love Mountains...</p>
+              <p id="">{data.description}</p>
               <a href="" uk-icon="heart" />
               <a href="" uk-icon="comments" />
             </div>
